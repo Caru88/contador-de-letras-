2f52cf12a1db7681fb909fd64464a0a52e74730a
Caru88 <joaquinbusaniche@gmail.com>
2025-08-03 16:38:54 -0300

    es un contador de palabras en base a un algoritmo

---
+++ contador-letras.py
@@ -0,0 +1,11 @@
+# paso 1: //solicitar entrada al usuario
+
+palabra_ingresada = input("por favor, ingrese una palabra: ")
+
+# paso 2: // contar la cantidad de letras
+
+cantidad_letras = len(palabra_ingresada)
+
+#  Paso 3: //mostrar al usuario el resultado 
+
+print("La cantidad de letras en la palabra ingresada es:", cantidad_letras)
